2186a10ce256c4cf89b69292850237d3f9b612ce
Michal Zaduban <51057141+zadubanm02@users.noreply.github.com>
2023-07-19 20:58:14 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,7 @@ const activeChain = "goerli";
 function MyApp({ Component, pageProps }: AppProps) {
   const startMoralis = async () => {
     await Moralis.start({
-      apiKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImY1MWNmMzgzLTU2NDYtNDE4MC04Y2FjLTUwOTAyYzZiZGYzOSIsIm9yZ0lkIjoiMzE3NzIyIiwidXNlcklkIjoiMzI2NjU0IiwidHlwZUlkIjoiM2NlMjQ5YWUtZDBjYy00ZWFkLWIxYjEtYmJjZGQ5MTFmMmUzIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODQ1OTI0NjMsImV4cCI6NDg0MDM1MjQ2M30.ryDWoyjZ3-V2hkFmsZI3WLiKykroFst-vmCqhOdqSEQ",
+      apiKey",
     });
   };
   useEffect(() => {
